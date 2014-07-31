dbd0ac714a2de1f0acdded13ab5ac06bc0f84ccc
Kristoffer Ørum <kristoffer@oerum.org>
2014-07-31 23:10:57 +0200

    baby steps

--- esoh-jquery-plugin-skeleton source/src/jquery.square.js
+++ esoh-jquery-plugin-skeleton source/src/jquery.square.js
@@ -1,7 +1,8 @@
 // the semi-colon before the function invocation is a safety 
 // net against concatenated scripts and/or other plugins 
 // that are not closed properly.
-;(function ( $, window, document ) {
+;
+(function($, window, document) {
 
     'use strict';
 
@@ -11,8 +12,8 @@
 
         // default options, used for instantion, if not explicitly set
         defaults = {
-            nr     : 0,
-            onLoad : function() {
+            nr: 0,
+            onLoad: function() {
                 console.info('default onLoad() event');
             }
         };
@@ -57,7 +58,7 @@
 
         // override default options
         // create a new object, with all default settings, overridden only by the init options
-        this.options = $.extend( {}, defaults, options );
+        this.options = $.extend({}, defaults, options);
     };
 
 
@@ -72,7 +73,7 @@
      *  @param   {Number}  nr  the input number
      *  @returns {Number}
      */
-    var _getSquare = function (nr) {
+    var _getSquare = function(nr) {
         return nr * nr;
     };
 
@@ -90,19 +91,19 @@
          *
          *  @private
          */
-        init: function () {
+        init: function() {
             var _this = this;
 
             // add the other input, to display the calculated square
-            _this.elementSquare = $('<input readonly name="' + pluginName + '">').insertAfter( _this.element );
+            _this.elementSquare = $('<input readonly name="' + pluginName + '">').insertAfter(_this.element);
 
             // bind events
             _this.element.on('keyup', function() {
-                _this.elementSquare.val( _getSquare( $(this).val() ) );
+                _this.elementSquare.val(_getSquare($(this).val()));
             });
 
             // set initial value, and trigger the 'keyup', to update the square
-            _this.element.val( _this.options.nr ).trigger('keyup');
+            _this.element.val(_this.options.nr).trigger('keyup');
 
             // trigger onLoad callback
             _this.options.onLoad();
@@ -113,7 +114,7 @@
          *
          *  @example instance.square('getValue');
          */
-        getValue: function () {
+        getValue: function() {
             return this.element.val();
         },
 
@@ -124,18 +125,18 @@
          *
          *  @example instance.square('setValue', 2);
          */
-        setValue: function (nr) {
+        setValue: function(nr) {
             var _nr = parseInt(nr, 10);
 
-            if ( isNaN(_nr) ) {
+            if (isNaN(_nr)) {
                 return false;
             }
 
-            if ( _nr < 0 ) {
+            if (_nr < 0) {
                 _nr = 0;
             }
 
-            this.element.val( _nr );
+            this.element.val(_nr);
         }
     };
 
@@ -146,41 +147,41 @@
 
 
     // A plugin wrapper around the constructor, preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
+    $.fn[pluginName] = function(options) {
         var instance;
 
         // If the first parameter is an object (options), or was omitted,
         // call Plugin.init()
-        if ( typeof options === 'undefined' || typeof options === 'object' ) {
-            return this.each(function () {
+        if (typeof options === 'undefined' || typeof options === 'object') {
+            return this.each(function() {
                 // prevent multiple instantiations
-                if ( !$.data(this, dataPlugin )) {
+                if (!$.data(this, dataPlugin)) {
                     $.data(
-                        this, 
-                        dataPlugin, 
-                        new Plugin( this, options )
+                        this,
+                        dataPlugin,
+                        new Plugin(this, options)
                     );
                 }
 
-                instance = $(this).data( dataPlugin );
+                instance = $(this).data(dataPlugin);
 
-                if ( typeof instance['init'] === 'function' ) {
+                if (typeof instance['init'] === 'function') {
                     instance.init();
                 }
             });
 
-        // checks that the requested public method exists
-        } else if ( typeof options === 'string' ) {
+            // checks that the requested public method exists
+        } else if (typeof options === 'string') {
             var methodName = arguments[0],
                 args = Array.prototype.slice.call(arguments, 1),
                 returnVal;
 
             this.each(function() {
-                var instance = $(this).data( dataPlugin );
+                var instance = $(this).data(dataPlugin);
 
                 // Check that the element has a plugin instance, and that
                 // the requested public method exists.
-                if ( $.data(this, dataPlugin) && typeof $.data(this, dataPlugin)[methodName] === 'function' ) {
+                if ($.data(this, dataPlugin) && typeof $.data(this, dataPlugin)[methodName] === 'function') {
                     // Call the method of the Plugin instance, and Pass it
                     // the supplied arguments.
                     returnVal = $.data(this, dataPlugin)[methodName].apply(instance, args);
@@ -189,7 +190,7 @@
                 }
             });
 
-            if ( typeof returnVal !== 'undefined' ){
+            if (typeof returnVal !== 'undefined') {
                 // If the method returned a value, return the value
                 return returnVal;
             } else {
@@ -200,4 +201,4 @@
             console.info('Method ' + options + ' does not exist on jQuery.' + pluginName);
         }
     };
-})( jQuery, window, document );
+})(jQuery, window, document);
